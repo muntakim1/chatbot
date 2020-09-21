344982df970d69ee465ae02c8d2dcc56e7e04924
muntakim <muntakim1104001@gmail.com>
2020-09-21 22:27:31 +0600

    new contexted added

--- chatbot.py
+++ chatbot.py
@@ -4,5 +4,7 @@ kernel = Kernel()
 
 kernel.learn('brain.aiml')
 
+print("Say Hello! and ask questions like, I want to know about loan")
+
 while True:
     print("Bot->",kernel.respond(input("User-> ")))
